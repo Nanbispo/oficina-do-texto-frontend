9b496701ed4e58e18ba3e24ee450548bc960ca63
Renan Bispo <renanjesusbispo@gmail.com>
2026-07-18 22:10:44 -0300

    passando direto

--- src/api/client.ts
+++ src/api/client.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const api = axios.create({
-  baseURL : import.meta.env.VITE_API_URL
+  baseURL : 'https://app-go-389874839960.us-central1.run.app'
 });
 
 api.interceptors.request.use(
