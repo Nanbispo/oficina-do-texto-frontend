9425fff0430d8f70dbb1544799f3a53c9d1e5810
Renan Bispo <renanjesusbispo@gmail.com>
2026-07-18 21:24:37 -0300

    variaveis de ambiente para producao

--- src/api/client.ts
+++ src/api/client.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const api = axios.create({
-  baseURL: '/api', // 🌟 Mude para isso! Sem localhost e sem a URL do Cloud Run aqui.
+  baseURL: import.meta.env.VITE_API_URL, 
 });
 
 api.interceptors.request.use(
